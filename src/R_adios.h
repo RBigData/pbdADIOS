c5f93b5db6116e9e890c84816f193c453e2ebc40
Drew Schmidt <schmidt@math.utk.edu>
2014-08-29 19:09:45 -0400

    removing garbage includes

--- src/R_adios.h
+++ src/R_adios.h
@@ -10,11 +10,11 @@
 
 //#include "adios/src/public/adios.h"
 
-#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios.h"
-#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_error.h"
-#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_read.h"
-#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_selection.h"
-#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_types.h"
+//#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios.h"
+//#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_error.h"
+//#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_read.h"
+//#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_selection.h"
+//#include "/sw/redhat6/adios/1.7.0/rhel6_gnu4.7.1/include/adios_types.h"
 
 /* Obtain character pointers. */
 #define CHARPT(x,i)     ((char*)CHAR(STRING_ELT(x,i)))

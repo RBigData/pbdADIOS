adb7df8114c39770bfafb9f3ae3dbfa19272d907
wrathematics <wrathematics@gmail.com>
2015-05-06 13:01:31 -0400

    don't unload, do export, don't first.lib

--- R/zzz.r
+++ R/zzz.r
@@ -1,17 +1,4 @@
-### Lastest load into a package.
-
-.First.lib <- function(lib, pkg){
-  if(! is.loaded("spmd_initialize", PACKAGE = "pbdMPI")){
-    library.dynam("pbdMPI", "pbdMPI", lib)
-    if(pbdMPI::comm.is.null(0L) == -1){
-      pbdMPI::init()
-    }
-  }
-
-  library.dynam("pbdADIOS", pkg, lib)
-} # End of .First.lib().
-
+#' @export
 .Last.lib <- function(libpath){
   pbdADIOS::adios.finalize(pbdMPI::comm.rank())
-  library.dynam.unload("pbdADIOS", libpath)
-} # End of .Last.lib().
+} 

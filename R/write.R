5fb69a6f7ee799b78b897a80dafbc9469894d594
Allan Yuping Lu <yupinglu@outlook.com>
2016-06-14 16:28:02 -0400

    Add write.R

---
+++ R/write.R
@@ -0,0 +1,106 @@
+adios.init.noxml <- function(comm = pbdMPI::.pbd_env$SPMD.CT$comm)
+{
+    .Call(R_adios_init_noxml, 
+          comm.c2f(comm))
+    invisible()
+}
+
+adios.allocate.buffer <- function(adios.buffer.when,
+                                  adios.buffersize)
+{
+    .Call(R_adios_allocate_buffer, 
+          as.character(adios.buffer.when),
+          as.integer(adios.buffersize))  
+    invisible()
+} 
+
+adios.declare.group <- function(adios.groupname, 
+                                adios.timeindex,
+                                adios.flag)
+{
+    .Call(R_adios_declare_group, 
+          as.character(adios.groupname), 
+          as.character(adios.timeindex),
+          as.character(adios.flag))
+}
+
+adios.select.method <- function(adios.group_p, 
+                                adios.method, 
+                                adios.params, 
+                                adios.basepath)
+{
+        
+    .Call(R_adios_select_method, 
+          adios.group_p, 
+          as.character(adios.method), 
+          as.character(adios.params), 
+          as.character(adios.basepath))        
+    invisible()
+}
+
+adios.define.var <- function(adios.group_p, 
+                             adios.varname, 
+                             adios.path, 
+                             adios.type,
+                             adios.localdim, 
+                             adios.globaldim, 
+                             adios.localoffset )
+{
+  
+    .Call(R_adios_define_var,
+          adios.group_p, 
+          as.character(adios.varname), 
+          as.character(adios.path), 
+          as.character(adios.type),
+          as.character(adios.localdim), 
+          as.character(adios.globaldim), 
+          as.character(adios.localoffset))
+    invisible()
+}
+
+adios.open <- function(adios_fd,
+                       adios.groupname, 
+                       adios.filename, 
+                       adios.mode, 
+                       comm = pbdMPI::.pbd_env$SPMD.CT$comm)
+{
+  
+    .Call(R_adios_open,
+          adios_fd,
+          as.character(adios.groupname), 
+          as.character(adios.filename), 
+          as.character(adios.mode), 
+          comm.c2f(comm))
+}
+
+adios.group.size <- function(adios.file_p, 
+                             adios.groupsize)
+{
+    .Call(R_adios_group_size, 
+          adios.file_p, 
+          as.numeric(adios.groupsize))
+}
+
+adios.write <- function(adios.file_p, 
+                        adios.varname, 
+                        adios.var)
+{
+    .Call(R_adios_write,
+          adios.file_p, 
+          as.character(adios.varname), 
+          adios.var)
+}
+
+adios.close <- function(adios.file_p)
+{
+    .Call(R_adios_close, 
+          adios.file_p)
+    invisible()
+}
+
+adios.finalize <- function(comm = pbdMPI::.pbd_env$SPMD.CT$comm)
+{
+    .Call(R_adios_finalize, 
+          comm.rank(comm))
+    invisible()
+}

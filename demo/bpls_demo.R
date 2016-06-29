4cc3bc5242513de3bf30ec54571f4f6500dd6654
Allan Yuping Lu <yupinglu@outlook.com>
2016-06-29 10:37:07 -0400

    fix bpls_demo.R

--- demo/bpls_demo.R
+++ demo/bpls_demo.R
@@ -7,8 +7,12 @@ library(pbdADIOS, quiet = TRUE)
 init() # pbdMPI initilization
 
 cat("rank ",comm.rank(.pbd_env$SPMD.CT$comm), "start\n")
-bpls("heat.bp", .pbd_env$SPMD.CT$comm, comm.rank(.pbd_env$SPMD.CT$comm))
-barrier()
+
+#only rank 0 print the result
+if(comm.rank(.pbd_env$SPMD.CT$comm) == 0) {
+    bpls("heat.bp", .pbd_env$SPMD.CT$comm, comm.rank(.pbd_env$SPMD.CT$comm))
+}
+
 cat("rank ",comm.rank(.pbd_env$SPMD.CT$comm), "finish\n")
 
 finalize() # pbdMPI final

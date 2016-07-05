a2ad3f9d0c540595d4d710216601e95c74a550f2
Allan Yuping Lu <yupinglu@outlook.com>
2016-07-05 01:08:02 -0400

    remove unused methods

--- src/R_dump.h
+++ src/R_dump.h
@@ -1,38 +1,6 @@
 #ifndef __R_DUMP__
 #define __R_DUMP__
 
-/*#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-
-#include <mpi.h>
-
-#include <R.h>
-#include <Rdefines.h>
-#include <Rinternals.h>
-
-#include <adios.h>
-#include <adios_read.h>
-#include <inttypes.h>
-#include <stdbool.h>
-
-
-#ifdef DMALLOC
-#include "dmalloc.h"
-#endif
-
-
-#define INT(x) INTEGER(x)[0]
-#define newRptr(ptr,Rptr,fin) PROTECT(Rptr = R_MakeExte-rnalPtr(ptr, R_NilValue, R_NilValue)); R_RegisterCFinalizerEx(Rptr, fin, TRUE)
- /* newRptr(already_allocated_C_pointer, R_pointer_to_be_made_for_it, finalizer) */
-/*#define CHARPT(x,i)     ((char*)CHAR(STRING_ELT(x,i)))
-
-void mergeLists(int nV, char **listV, int nA, char **listA, char **mlist, bool *isVar);
-int print_data(void *data, int item, enum ADIOS_DATATYPES adiosvartype);
-int doList_group (ADIOS_FILE *fp);
-*/
-
 #define MAX_DIMS 16
 #define MAX_MASKS 10
 #define MAX_BUFFERSIZE (10*1024*1024)

13e28eaa6faa22b26bc7fddff48b5f5bd5e10874
YupingLu <allanflytosky@gmail.com>
2016-07-07 13:05:33 -0400

    update read method

--- src/R_read.h
+++ src/R_read.h
@@ -1,12 +1,11 @@
 #ifndef __R_READ__
 #define __R_READ__
 
-dump_var (SEXP R_adios_fp,
-          SEXP R_varname);
+SEXP dump_var (SEXP R_adios_fp,
+               SEXP R_varname);
 
-R_read(SEXP R_adios_path,
-       SEXP R_varname,
-       SEXP R_comm,
-       SEXP R_adios_rank);
-
+SEXP R_read(SEXP R_adios_path,
+            SEXP R_varname,
+            SEXP R_comm,
+            SEXP R_adios_rank);
 #endif
